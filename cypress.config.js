9377e9b882625cffbe3c3c9b1fad0e14289ee260
Srishanthini Maheswaran <srishanthini@tinisoft.in>
2025-02-02 11:21:42 +0530

    First Commit

---
+++ cypress.config.js
@@ -0,0 +1,265 @@
+// //LOGIN VALIDATION
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in/',
+//     env: {
+//       apiUrl: 'https://dev.office.tinisoft.in/accounts/signin',
+//       username: 'super@tsoffice.com',
+//       password: 'Test@123'
+//     },
+//     // other configurations...
+//   },
+// };
+
+// //TODO VALIDATION
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User2: {
+//         username: 'srishanthini@tinisoft.in',
+//         password: 'Shansandy@2309'
+//       },
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       }
+//     }
+//   }
+// };
+
+
+
+// //NOTES VALIDATION
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+
+
+// // //Asset Validation
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+// //Customers Validation
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+
+// //Vendors Validation
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+
+// //Users Validation
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+
+// //Secure Notes 
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+
+
+// // Bank Account 
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+
+//  //INVOICE
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsofc.in',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+
+
+//  //Employees
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+
+// //Queries
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+// //Recruitment
+// // cypress.config.js
+// module.exports = {
+//   e2e: {
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//         username: 'super@tsoffice.com',
+//         password: 'Test@123'
+//       },
+//       User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               }
+//     }
+//   }
+// };
+
+
+// //Ledgers
+// //cypress.config.js
+// module.exports = {
+//   e2e: {
+//     experimentalMemoryManagement: true,
+//     numTestsKeptInMemory: 1,
+//     baseUrl: 'https://dev.office.tinisoft.in',
+//     env: {
+//       User1: {
+//                 username: 'super@tsofc.in',
+//                 password: 'Test@123'
+//               },
+              
+//               User2: {
+//                 username: 'srishanthini@tinisoft.in',
+//                 password: 'Shansandy@2309'
+//               },
+//     }
+//   }
+// };
+
+
